15c42f98051e140b0343b797260a6c827b567937
michel <diezvivar@gmail.com>
2018-12-10 21:27:57 +0100

    Updates

---
+++ precache-manifest.ad693cd06c40463d10ccc91cef2c2d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e8e625f12e45ecc674f9579c4547351",
+    "url": "/static/media/DS-DIGII.8e8e625f.TTF"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15c74b3126a606affbbe",
+    "url": "/static/js/main.15c74b31.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "15c74b3126a606affbbe",
+    "url": "/static/css/main.bf206280.chunk.css"
+  },
+  {
+    "revision": "3c2b8299a7fca7f451dae8e7a2bf11f2",
+    "url": "/index.html"
+  }
+];
